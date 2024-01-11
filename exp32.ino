c9b23edacaa2ab367f96b697be71b3c205ff7418
Saker233 <mohamedsaker233@gmail.com>
2024-01-11 19:05:54 +0200

    the code which control the whole project

---
+++ exp32.ino
@@ -0,0 +1,147 @@
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <WebSocketsServer.h>
+#include "index.h"
+
+#define CMD_STOP 0
+#define CMD_FORWARD 1
+#define CMD_BACKWARD 2
+#define CMD_LEFT 4
+#define CMD_RIGHT 8
+
+#define ENA_PIN 5  
+#define IN1_PIN 13  
+#define IN2_PIN 12  
+#define IN3_PIN 14 
+#define IN4_PIN 27  
+#define ENB_PIN 18  
+
+const char* ssid = "Ahmed Saker";     
+const char* password = "ahmedgamal"; 
+
+AsyncWebServer server(80);
+WebSocketsServer webSocket = WebSocketsServer(81);  
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t* payload, size_t length) {
+  switch (type) {
+    case WStype_DISCONNECTED:
+      Serial.printf("[%u] Disconnected!\n", num);
+      break;
+    case WStype_CONNECTED:
+      {
+        IPAddress ip = webSocket.remoteIP(num);
+        Serial.printf("[%u] Connected from %d.%d.%d.%d\n", num, ip[0], ip[1], ip[2], ip[3]);
+      }
+      break;
+    case WStype_TEXT:
+      
+      String angle = String((char*)payload);
+      int command = angle.toInt();
+      Serial.print("command: ");
+      Serial.println(command);
+
+      switch (command) {
+        case CMD_STOP:
+          Serial.println("Stop");
+          CAR_stop();
+          break;
+        case CMD_FORWARD:
+          Serial.println("Move Forward");
+          CAR_moveForward();
+          break;
+        case CMD_BACKWARD:
+          Serial.println("Move Backward");
+          CAR_moveBackward();
+          break;
+        case CMD_LEFT:
+          Serial.println("Turn Left");
+          CAR_turnLeft();
+          break;
+        case CMD_RIGHT:
+          Serial.println("Turn Right");
+          CAR_turnRight();
+          break;
+        default:
+          Serial.println("Unknown command");
+      }
+
+      break;
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(ENA_PIN, OUTPUT);
+  pinMode(IN1_PIN, OUTPUT);
+  pinMode(IN2_PIN, OUTPUT);
+  pinMode(IN3_PIN, OUTPUT);
+  pinMode(IN4_PIN, OUTPUT);
+  pinMode(ENB_PIN, OUTPUT);
+
+  digitalWrite(ENA_PIN, HIGH);  
+  digitalWrite(ENB_PIN, HIGH);  
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
+
+  
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
+    Serial.println("Web Server: received a web page request");
+    String html = HTML_CONTENT;  
+    request->send(200, "text/html", html);
+  });
+
+  server.begin();
+  Serial.print("ESP32 Web Server's IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  webSocket.loop();
+  
+}
+
+void CAR_moveForward() {
+  digitalWrite(IN1_PIN, HIGH);
+  digitalWrite(IN2_PIN, LOW);
+  digitalWrite(IN3_PIN, HIGH);
+  digitalWrite(IN4_PIN, LOW);
+}
+
+void CAR_moveBackward() {
+  digitalWrite(IN1_PIN, LOW);
+  digitalWrite(IN2_PIN, HIGH);
+  digitalWrite(IN3_PIN, LOW);
+  digitalWrite(IN4_PIN, HIGH);
+}
+
+void CAR_turnLeft() {
+  digitalWrite(IN1_PIN, HIGH);
+  digitalWrite(IN2_PIN, LOW);
+  digitalWrite(IN3_PIN, LOW);
+  digitalWrite(IN4_PIN, LOW);
+}
+
+void CAR_turnRight() {
+  digitalWrite(IN1_PIN, LOW);
+  digitalWrite(IN2_PIN, LOW);
+  digitalWrite(IN3_PIN, HIGH);
+  digitalWrite(IN4_PIN, LOW);
+}
+
+void CAR_stop() {
+  digitalWrite(IN1_PIN, LOW);
+  digitalWrite(IN2_PIN, LOW);
+  digitalWrite(IN3_PIN, LOW);
+  digitalWrite(IN4_PIN, LOW);
+}
